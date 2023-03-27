8f9fc71854c0c9e1d8805792ba39f26380315ff1
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-27 21:08:16 +0700

    Update utilities.ps1

--- utilities.ps1
+++ utilities.ps1
@@ -1,123 +1,181 @@
-param
-(
-	[switch]$all,
-	[switch]$store,
-	[switch]$office,
-	[switch]$spotx,
-	[switch]$dpi,
-	[switch]$directx,
-	[switch]$vcredist,
-	[switch]$chrome,
-	[switch]$discord,
-	[switch]$steam,
-	[switch]$qbit,
-	[switch]$zip,
-	[switch]$gdrive,
-	[switch]$adguard,
-	[switch]$blender,
-	[switch]$signal,
-	[switch]$codec,
-	[switch]$nvidia
-)
+<#
+	Запуск скрипта с интерактивным выбором автоматической/ручной установки:
+	&{irm https://raw.githubusercontent.com/uffemcev/utilities/main/utilities.ps1; manual} | iex
+	&{ . .\utilities.ps1; manual}
+	
+	Автоматическая установка выбранных компонентов:
+	& { . .\utilities1.ps1; auto -store -office -chrome }
+	
+	Автоматическая установка всех компонентов:
+	& { . .\utilities1.ps1; auto -all }
+	
 
-if ($all)
+	Download.ps1 -Branch O365ProPlusRetail -Channel SemiAnnual -Components Excel, OneDrive, Outlook, PowerPoint, Teams, Word
+	.EXAMPLE
+	Download.ps1 -Branch O365ProPlusRetail -Channel BetaChannel -Components Excel, Word
+	.NOTES
+	Run as admin
+#>
+function manual
 {
-	$store = $office = $spotx = $dpi = $directx = $vcredist = $chrome = $discord = $steam = $qbit = $zip = $gdrive = $adguard = $blender = $signal = $codec = $nvidia = $True
+	$Number = Read-Host "Please enter a number"
+	if ($Number -eq 0) {choose}
+	if ($Number -eq 1) {$all = $True}
+	run
 }
 
-if ($store)
+function auto
 {
-	Get-CimInstance -Namespace 'root\cimv2\mdm\dmmap' -ClassName 'MDM_EnterpriseModernAppManagement_AppManagement01' | Invoke-CimMethod -MethodName UpdateScanMethod
+	param
+	(
+		[switch]$all,
+		[switch]$store,
+		[switch]$office,
+		[switch]$spotx,
+		[switch]$dpi,
+		[switch]$directx,
+		[switch]$vcredist,
+		[switch]$chrome,
+		[switch]$discord,
+		[switch]$steam,
+		[switch]$qbit,
+		[switch]$zip,
+		[switch]$gdrive,
+		[switch]$adguard,
+		[switch]$blender,
+		[switch]$signal,
+		[switch]$codec,
+		[switch]$nvidia
+	)
+	run
 }
 
-if ($office)
+function choose
 {
-	iwr 'https://github.com/farag2/Office/releases/latest/download/Office.zip' -OutFile '.\Office.zip'
-	Expand-Archive '.\Office.zip' '.\'
-	ri '.\Office.zip'
-	pushd '.\Office'
-	iex '.\Download.ps1 -Branch O365ProPlusRetail -Channel Current -Components Word, Excel, PowerPoint'
-	iex '.\Install.ps1'
-	& ([ScriptBlock]::Create((irm https://massgrave.dev/get))) /KMS-Office /KMS-ActAndRenewalTask /S
-	popd
-	ri -Recurse '.\Office'
+	param
+	(
+		[Parameter(Mandatory = $true)][int]$store,
+		[Parameter(Mandatory = $true)][int]$office,
+		[Parameter(Mandatory = $true)][int]$spotx,
+		[Parameter(Mandatory = $true)][int]$dpi,
+		[Parameter(Mandatory = $true)][int]$directx,
+		[Parameter(Mandatory = $true)][int]$vcredist,
+		[Parameter(Mandatory = $true)][int]$chrome,
+		[Parameter(Mandatory = $true)][int]$discord,
+		[Parameter(Mandatory = $true)][int]$steam,
+		[Parameter(Mandatory = $true)][int]$qbit,
+		[Parameter(Mandatory = $true)][int]$zip,
+		[Parameter(Mandatory = $true)][int]$gdrive,
+		[Parameter(Mandatory = $true)][int]$adguard,
+		[Parameter(Mandatory = $true)][int]$blender,
+		[Parameter(Mandatory = $true)][int]$signal,
+		[Parameter(Mandatory = $true)][int]$codec,
+		[Parameter(Mandatory = $true)][int]$nvidia
+	)
+	run
 }
 
-if ($spotx)
+function run
 {
-	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-	iex "& { $((iwr -useb 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content) } -premium -new_theme -podcasts_on -block_update_on -cache_on"
-}
+	if ($all)
+	{
+		option -store -office -spotx -dpi -directx -vcredist -chrome -discord -steam -qbit -zip -gdrive -adguard -blender -signal -codec -nvidia
+	}
 
-if ($dpi)
-{
-	iwr 'https://github.com/ValdikSS/GoodbyeDPI/releases/latest/download/goodbyedpi-0.2.2.zip' -OutFile '.\goodbyedpi.zip'
-	Expand-Archive '.\goodbyedpi.zip' $Env:Programfiles
-	Remove-Item '.\goodbyedpi.zip'
-	dir -Path $Env:Programfiles -Recurse -ErrorAction SilentlyContinue -Force | where {$_ -in '0_russia_update_blacklist_file.cmd','service_install_russia_blacklist.cmd'} | %{ '`n' |& $_.FullName }
-}
+	if ($store)
+	{
+		Get-CimInstance -Namespace 'root\cimv2\mdm\dmmap' -ClassName 'MDM_EnterpriseModernAppManagement_AppManagement01' | Invoke-CimMethod -MethodName UpdateScanMethod
+	}
 
-if ($directx)
-{
-	winget install --id=Microsoft.DirectX --accept-package-agreements --accept-source-agreements --exact --silent
-}
+	if ($office)
+	{
+		iwr 'https://github.com/farag2/Office/releases/latest/download/Office.zip' -OutFile '.\Office.zip'
+		Expand-Archive '.\Office.zip' '.\'
+		ri '.\Office.zip'
+		pushd '.\Office'
+		iex '.\Download.ps1 -Branch O365ProPlusRetail -Channel Current -Components Word, Excel, PowerPoint'
+		iex '.\Install.ps1'
+		& ([ScriptBlock]::Create((irm https://massgrave.dev/get))) /KMS-Office /KMS-ActAndRenewalTask /S
+		popd
+		ri -Recurse '.\Office'
+	}
 
-if ($vcredist)
-{
-	winget install --id=Microsoft.VCRedist.2015+.x64 --accept-package-agreements --accept-source-agreements --exact --silent
-}
+	if ($spotx)
+	{
+		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+		iex "& { $((iwr -useb 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content) } -premium -new_theme -podcasts_on -block_update_on -cache_on"
+	}
 
-if ($chrome)
-{
-	winget install --id=Google.Chrome --accept-package-agreements --accept-source-agreements --exact --silent
-}
+	if ($dpi)
+	{
+		iwr 'https://github.com/ValdikSS/GoodbyeDPI/releases/latest/download/goodbyedpi-0.2.2.zip' -OutFile '.\goodbyedpi.zip'
+		Expand-Archive '.\goodbyedpi.zip' $Env:Programfiles
+		Remove-Item '.\goodbyedpi.zip'
+		dir -Path $Env:Programfiles -Recurse -ErrorAction SilentlyContinue -Force | where {$_ -in '0_russia_update_blacklist_file.cmd','service_install_russia_blacklist.cmd'} | %{ '`n' |& $_.FullName }
+	}
 
-if ($discord)
-{
-	winget install --id=Discord.Discord --accept-package-agreements --accept-source-agreements --exact --silent
-}
+	if ($directx)
+	{
+		winget install --id=Microsoft.DirectX --accept-package-agreements --accept-source-agreements --exact --silent
+	}
 
-if ($steam)
-{
-	winget install --id=Valve.Steam --accept-package-agreements --accept-source-agreements --exact --silent
-}
+	if ($vcredist)
+	{
+		winget install --id=Microsoft.VCRedist.2015+.x64 --accept-package-agreements --accept-source-agreements --exact --silent
+	}
 
-if ($qbit)
-{
-	winget install --id=qBittorrent.qBittorrent --accept-package-agreements --accept-source-agreements --exact --silent
-}
+	if ($chrome)
+	{
+		winget install --id=Google.Chrome --accept-package-agreements --accept-source-agreements --exact --silent
+	}
 
-if ($zip)
-{
-	winget install --id=7zip.7zip --accept-package-agreements --accept-source-agreements --exact --silent
-}
+	if ($discord)
+	{
+		winget install --id=Discord.Discord --accept-package-agreements --accept-source-agreements --exact --silent
+	}
 
-if ($gdrive)
-{
-	winget install --id=Google.Drive --accept-package-agreements --accept-source-agreements --exact --silent
-}
+	if ($steam)
+	{
+		winget install --id=Valve.Steam --accept-package-agreements --accept-source-agreements --exact --silent
+	}
 
-if ($adguard)
-{
-	winget install --id=AdGuard.AdGuard --accept-package-agreements --accept-source-agreements --exact --silent
-}
+	if ($qbit)
+	{
+		winget install --id=qBittorrent.qBittorrent --accept-package-agreements --accept-source-agreements --exact --silent
+	}
 
-if ($blender)
-{
-	winget install --id=BlenderFoundation.Blender --accept-package-agreements --accept-source-agreements --exact --silent
-}
+	if ($zip)
+	{
+		winget install --id=7zip.7zip --accept-package-agreements --accept-source-agreements --exact --silent
+	}
 
-if ($signal)
-{
-	winget install --id=WhirlwindFX.SignalRgb --accept-package-agreements --accept-source-agreements --exact --silent
-}
+	if ($gdrive)
+	{
+		winget install --id=Google.Drive --accept-package-agreements --accept-source-agreements --exact --silent
+	}
 
-if ($codec)
-{
-	winget install --id=CodecGuide.K-LiteCodecPack.Full --accept-package-agreements --accept-source-agreements --exact --interactive
-}
+	if ($adguard)
+	{
+		winget install --id=AdGuard.AdGuard --accept-package-agreements --accept-source-agreements --exact --silent
+	}
 
-if ($nvidia)
-{
-	winget install --id=TechPowerUp.NVCleanstall --accept-package-agreements --accept-source-agreements --exact --interactive
+	if ($blender)
+	{
+		winget install --id=BlenderFoundation.Blender --accept-package-agreements --accept-source-agreements --exact --silent
+	}
+
+	if ($signal)
+	{
+		winget install --id=WhirlwindFX.SignalRgb --accept-package-agreements --accept-source-agreements --exact --silent
+	}
+
+	if ($codec)
+	{
+		winget install --id=CodecGuide.K-LiteCodecPack.Full --accept-package-agreements --accept-source-agreements --exact --interactive
+	}
+
+	if ($nvidia)
+	{
+		winget install --id=TechPowerUp.NVCleanstall --accept-package-agreements --accept-source-agreements --exact --interactive
+	}
 }
