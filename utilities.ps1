dbd9718440ada804838fc9e3549f1c3e8d7d7349
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-28 00:27:15 +0700

    Update utilities.ps1

--- utilities.ps1
+++ utilities.ps1
@@ -9,19 +9,19 @@
 	Ручной выбор компонентов для установки:
 	powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; &{iex (iwr https://raw.githubusercontent.com/uffemcev/utilities/main/utilities.ps1); manual}"
 	powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; &{. .\utilities.ps1; manual}"
-	&{iex (iwr https://raw.githubusercontent.com/uffemcev/utilities/main/utilities.ps1); manual}
+	Set-ExecutionPolicy Bypass -Scope Process -Force; &{iex (iwr https://raw.githubusercontent.com/uffemcev/utilities/main/utilities.ps1); manual}
 	Set-ExecutionPolicy Bypass -Scope Process -Force; &{. .\utilities.ps1; manual}
 	
 	Автоматическая установка указанных компонентов:
 	powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; &{iex (iwr https://raw.githubusercontent.com/uffemcev/utilities/main/utilities.ps1); auto -store -office -chrome}"
 	powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; &{. .\utilities.ps1; auto -store -office -chrome}"
-	&{iex (iwr https://raw.githubusercontent.com/uffemcev/utilities/main/utilities.ps1); auto -store -office -chrome}
+	Set-ExecutionPolicy Bypass -Scope Process -Force; &{iex (iwr https://raw.githubusercontent.com/uffemcev/utilities/main/utilities.ps1); auto -store -office -chrome}
 	Set-ExecutionPolicy Bypass -Scope Process -Force; &{. .\utilities.ps1; auto -store -office -chrome}
 	
 	Автоматическая установка всех компонентов:
 	powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; &{iex (iwr https://raw.githubusercontent.com/uffemcev/utilities/main/utilities.ps1); auto -all}"
 	powershell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; &{. .\utilities.ps1; auto -all}"
-	&{iex (iwr https://raw.githubusercontent.com/uffemcev/utilities/main/utilities.ps1); auto -all}
+	Set-ExecutionPolicy Bypass -Scope Process -Force; &{iex (iwr https://raw.githubusercontent.com/uffemcev/utilities/main/utilities.ps1); auto -all}
 	Set-ExecutionPolicy Bypass -Scope Process -Force; &{. .\utilities.ps1; auto -all}
 #>
 function manual
