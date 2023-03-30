cc869c6a5d824db7748acccbf9e29a0647c32cd8
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-31 01:12:04 +0700

    Update and rename run10.ps1 to run1.ps1

--- run1.ps1
+++ run1.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-	$path = $MyInvocation.MyCommand.Name
+	$path = $MyInvocation.MyCommand
 	write-host $path
 	#Start-Process -FilePath '$MyInvocation.MyCommand'
 }
