6f7dfe986d4a8e98fdbfa9dc693cb6b6693d0637
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-30 23:52:55 +0700

    Update run3.ps1

--- run3.ps1
+++ run3.ps1
@@ -1,5 +1,57 @@
-if (!(net session)) {$path =  "& '" + $myinvocation.mycommand.definition + "'" ; Start-Process powershell -Verb runAs -ArgumentList $path ; exit}
+param(
+    [switch]$IsRunAsAdmin = $false
+)
 
+# Get our script path
+$ScriptPath = (Get-Variable MyInvocation).Value.MyCommand.Path
+
+#
+# Launches an elevated process running the current script to perform tasks
+# that require administrative privileges.  This function waits until the
+# elevated process terminates.
+#
+function LaunchElevated
+{
+    # Set up command line arguments to the elevated process
+    $RelaunchArgs = '-ExecutionPolicy Unrestricted -file "' + $ScriptPath + '" -IsRunAsAdmin'
+
+    # Launch the process and wait for it to finish
+    try
+    {
+        $AdminProcess = Start-Process "$PsHome\PowerShell.exe" -Verb RunAs -ArgumentList $RelaunchArgs -PassThru
+    }
+    catch
+    {
+        $Error[0] # Dump details about the last error
+        exit 1
+    }
+
+    # Wait until the elevated process terminates
+    while (!($AdminProcess.HasExited))
+    {
+        Start-Sleep -Seconds 2
+    }
+}
+
+function DoElevatedOperations
+{
+    Write-Host "Do elevated operations"
+}
+
+function DoStandardOperations
+{
+    Write-Host "Do standard operations"
+
+    LaunchElevated
+}
+
+
+#
+# Main script entry point
+#
+
+if ($IsRunAsAdmin)
+{
 cd $env:USERPROFILE
 
 function install([Array]$option)
@@ -114,3 +166,9 @@ $choose = Read-Host "`ngithub.com/uffemcev/utilities `n`n1 install everything `n
 if ($choose -eq 0) {install select}
 if ($choose -eq 1) {install all}
 } else {install $args}
+}
+
+else
+{
+    
+}
