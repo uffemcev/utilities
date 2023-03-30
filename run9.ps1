e7d51c931ad6448fc191ceb7f997686e74152fc7
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-31 00:24:45 +0700

    Update and rename run8.ps1 to run9.ps1

--- run9.ps1
+++ run9.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
-Start-Process powershell.exe -Verb RunAs "& '" "'"
+Start-Process powershell.exe -Verb RunAs "& '"
 }
 
 
