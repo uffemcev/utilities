8bb315619d5ec4be2857f506829ae9463f547ccd
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-31 03:54:58 +0700

    Update and rename run5.ps1 to run6.ps1

--- run6.ps1
+++ run6.ps1
@@ -1,7 +1,7 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
 	$path = $MyInvocation.line
-	Start-Process powershell "-NoExit -NoProfile -ExecutionPolicy Bypass -Command `"$path`"" -Verb RunAs; exit
+	Start-Process powershell "-NoExit -NoProfile -ExecutionPolicy Bypass -Command `"$path`"" -Verb RunAs
 }
 
 cd $env:USERPROFILE
