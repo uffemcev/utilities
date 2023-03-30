5f2c35852d8c41f99c32857ff485097f17dc3dfb
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-31 00:24:14 +0700

    Update run7.ps1

--- run7.ps1
+++ run7.ps1
@@ -1,4 +1,7 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe -Verb RunAs }
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+Start-Process powershell.exe -Verb RunAs "& '" "'"
+}
 
 
 
