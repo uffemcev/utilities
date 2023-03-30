943f9051e4aaa65f0ec18665cc4e69f207b64e44
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-30 23:30:45 +0700

    Update run3.ps1

--- run3.ps1
+++ run3.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
-    exit;
+    
 }
 
 function install([Array]$option)
