ca8f3f836075800c148c04a97f5dee08ef9a841a
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-31 00:38:13 +0700

    Update and rename run12.ps1 to run.ps1

--- run.ps1
+++ run.ps1
@@ -1,7 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-	Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"`";
-	
+	Start-Process PowerShell `"
 }
 
 cd $env:USERPROFILE
