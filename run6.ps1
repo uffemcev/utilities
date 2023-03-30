708dfbd118e7b2a8e074a9b0f47a2f19c0427d08
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-31 02:34:42 +0700

    Update and rename run5.ps1 to run6.ps1

--- run6.ps1
+++ run6.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-	write-host $MyInvocation.Line
+	write-host $MyInvocation
 	write-host $MyInvocation.MyCommand
 }
 
