dc7b673eabbdf864f5e0b9217fd2ddf19d38a506
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-31 00:57:50 +0700

    Update run3.ps1

--- run3.ps1
+++ run3.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-Start-Process $MyInvocation.MyCommand
+Start-Process -FilePath '$MyInvocation.MyCommand'
 }
 
 cd $env:USERPROFILE
