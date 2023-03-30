e2906c46ed1ac1443904341dc59588f6a277714e
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-31 00:29:55 +0700

    Update and rename run11.ps1 to run12.ps1

--- run12.ps1
+++ run12.ps1
@@ -1,35 +1,7 @@
-# Get the ID and security principal of the current user account
-$myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent();
-$myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($myWindowsID);
-
-# Get the security principal for the administrator role
-$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator;
-
-# Check to see if we are currently running as an administrator
-if ($myWindowsPrincipal.IsInRole($adminRole))
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-    # We are running as an administrator, so change the title and background colour to indicate this
-    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)";
-    $Host.UI.RawUI.BackgroundColor = "DarkBlue";
-    Clear-Host;
-}
-else {
-    # We are not running as an administrator, so relaunch as administrator
-
-    # Create a new process object that starts PowerShell
-    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
-
-    # Specify the current script path and name as a parameter with added scope and support for scripts with spaces in it's path
-    $newProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
-
-    # Indicate that the process should be elevated
-    $newProcess.Verb = "runas";
-
-    # Start the new process
-    [System.Diagnostics.Process]::Start($newProcess);
-
-    # Exit from the current, unelevated, process
-    Exit;
+	Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"`"";
+	exit;
 }
 
 cd $env:USERPROFILE
@@ -146,6 +118,3 @@ $choose = Read-Host "`ngithub.com/uffemcev/utilities `n`n1 install everything `n
 if ($choose -eq 0) {install select}
 if ($choose -eq 1) {install all}
 } else {install $args}
-
-Write-Host -NoNewLine "Press any key to continue...";
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
