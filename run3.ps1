de7c435a11666d0c620175e87a76cda86ba0b0db
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-30 23:34:11 +0700

    Update run3.ps1

--- run3.ps1
+++ run3.ps1
@@ -1,5 +1,5 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath'
     
 }
 
