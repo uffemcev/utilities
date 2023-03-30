7b58091b49afec957dafa3ac306fe60fd62d6720
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-31 00:49:02 +0700

    Update and rename run2.ps1 to run.ps1

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-	Start-Process $args
+	Start-Process powershell
 }
 
 cd $env:USERPROFILE
