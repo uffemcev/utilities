aa86f30a4186b9f1be6127798be8712bb5e73b97
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-03-31 02:31:49 +0700

    Update and rename run3.ps1 to run4.ps1

--- run4.ps1
+++ run4.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-	Start-Process $MyInvocation.Line
+	write-host $MyInvocation.Line
 }
 
 cd $env:USERPROFILE
