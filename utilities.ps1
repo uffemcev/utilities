6eb69451bc9cace233a726c2a55ee5b409258f3f
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-05-02 14:58:33 +0700

    Update utilities.ps1

--- utilities.ps1
+++ utilities.ps1
@@ -191,7 +191,6 @@ $data = @(
 		Code =
 		{
 			winget install --id=WhirlwindFX.SignalRgb --accept-package-agreements --accept-source-agreements --exact --silent
-			& ([ScriptBlock]::Create((irm raw.githubusercontent.com/uffemcev/rgb/main/rgb.ps1)))
 		}
 	}
 	@{
@@ -201,7 +200,6 @@ $data = @(
 		{
 			iwr 'https://github.com/uffemcev/OpenRGB/releases/download/0.81/OpenRGB.zip' -OutFile '.\OpenRGB.zip'
 			Expand-Archive '.\OpenRGB.zip' $env:APPDATA
-			& ([ScriptBlock]::Create((irm raw.githubusercontent.com/uffemcev/rgb/main/rgb.ps1)))
 		}
 	}
 	@{
