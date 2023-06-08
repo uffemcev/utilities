20ee84c52d3d62bf8bdcd59ebf003df685cbcbfe
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-06-09 01:21:31 +0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -31,7 +31,7 @@ if (Get-Process | where {$_.mainWindowTitle -match "uffemcev utilities|initializ
 {
 	"App is already running, try again soon"
 	start-sleep 5
-	Stop-Process -id $PID
+	get-process | where-object {$_.MainWindowTitle -eq $host.ui.RawUI.WindowTitle} | stop-process
 }
 
 #ПРОВЕРКА ИНТЕРНЕТА
