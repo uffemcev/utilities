b0c531108d3cb44e73eba8cfbdc5ca3ca5cc0062
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-06-15 14:19:56 +0700

    Update script5.ps1

--- script5.ps1
+++ script5.ps1
@@ -24,14 +24,16 @@ if (!(Get-NetAdapterStatistics))
 }
 
 #$MyInvocation.MyCommand.Path
-$MyInvocation.MyCommand.Definition
-pause
+#$MyInvocation.Line
+#pause
 
 #ПРОВЕРКА ПРАВ
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-	try {Start-Process wt "powershell -ExecutionPolicy Bypass -Command &{cd '$pwd'\; $($MyInvocation.line -replace (";"),("\;"))}" -Verb RunAs}
-	catch {Start-Process conhost "powershell -ExecutionPolicy Bypass -Command &{cd '$pwd'; $($MyInvocation.line)}" -Verb RunAs}
+	#try {Start-Process wt "powershell -ExecutionPolicy Bypass -Command cd '$pwd'\; $($MyInvocation.line -replace (";"),("\;"))" -Verb RunAs}
+	#try {Start-Process wt "-d $pwd powershell -ExecutionPolicy Bypass -Command $($MyInvocation.line)" -Verb RunAs}
+	try {Start-Process wt "powershell -ExecutionPolicy Bypass -Command $($MyInvocation.MyCommand.Definition)" -Verb RunAs}
+	catch {Start-Process conhost "powershell -ExecutionPolicy Bypass -Command cd '$pwd'; $($MyInvocation.line)" -Verb RunAs}
 	(get-process | where MainWindowTitle -eq $host.ui.RawUI.WindowTitle).id | where {taskkill /PID $_}
 }
 
