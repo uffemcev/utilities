6327e140f7d523da7e102af0d7233117842624a0
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-11-21 04:11:05 +0700

    Update apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -1,7 +1,8 @@
 @(
-	@{
+	[pscustomobject]@{
 		Description = "Cloudflare DOH"
 		Name = "dns"
+		Tag = "Tweaks"
 		Code = {
 			$ips = '1.1.1.1', '1.0.0.1', '2606:4700:4700::1111', '2606:4700:4700::1001'
 			$doh = "https://cloudflare-dns.com/dns-query"
@@ -18,9 +19,10 @@
 			Clear-DnsClientCache
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "Office, Word, Excel 365 licensed"
 		Name = "office"
+		Tag = "Programs"
 		Code = {
 			iwr 'https://github.com/farag2/Office/releases/latest/download/Office.zip' -Useb -OutFile '.\Office.zip'
 			Expand-Archive -ErrorAction 0 -Force '.\Office.zip' '.\'
@@ -32,17 +34,19 @@
 			popd
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "SpotX"
 		Name = "spotx"
+		Tag = "Programs"
 		Code = {
 			[Net.ServicePointManager]::SecurityProtocol = 3072
    			iex "& { $(iwr -useb 'https://spotx-official.github.io/run.ps1') } -premium -new_theme -podcasts_on -block_update_on"
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "GoodbyeDPI mode 5"
 		Name = "dpi"
+		Tag = "Programs"
 		Code = {
 			$uri = "https://api.github.com/repos/ValdikSS/GoodbyeDPI/releases/latest"
 			$get = Invoke-RestMethod -uri $uri -Method Get -ErrorAction stop
@@ -54,99 +58,110 @@
 			(iwr "https://reestr.rublacklist.net/api/v3/domains" -Useb) -split '", "' -replace ('[\[\]"]'), ('') | sc "$dir\russia-blacklist.txt"
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "Google Chrome"
 		Name = "chrome"
+		Tag = "Programs"
 		Code = {
 			$id = "Google.Chrome"
 			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --silent
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"}
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "Discord"
 		Name = "discord"
+		Tag = "Programs"
 		Code = {
 			$id = "Discord.Discord"
 			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --silent
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"}
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "Steam"
 		Name = "steam"
+		Tag = "Programs"
 		Code = {
 			$id = "Valve.Steam"
 			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --silent
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"}
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "qBittorrent"
 		Name = "qbit"
+		Tag = "Programs"
 		Code = {
 			$id = "qBittorrent.qBittorrent"
 			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --silent
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"}
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "7-Zip"
 		Name = "zip"
+		Tag = "Programs"
 		Code = {
 			$id = "7zip.7zip"
 			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --silent
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"}
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "Google Drive"
 		Name = "gdrive"
+		Tag = "Programs"
 		Code = {
 			$id = "Google.GoogleDrive"
 			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --silent
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"}
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "Adguard"
 		Name = "adguard"
+		Tag = "Programs"
 		Code = {
 			$id = "AdGuard.AdGuard"
 			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --silent
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"}
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "SignalRGB"
 		Name = "signal"
+		Tag = "Programs"
 		Code = {
 			$id = "WhirlwindFX.SignalRgb"
 			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --silent
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"}
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "K-Lite Codec Pack Full"
 		Name = "codec"
+		Tag = "Programs"
 		Code = {
 			$id = "CodecGuide.K-LiteCodecPack.Full"
-			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --interactive
+			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --silent
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"}
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "NVCleanstall"
 		Name = "nvidia"
+		Tag = "Drivers"
 		Code = {
 			$id = "TechPowerUp.NVCleanstall"
-			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --interactive
+			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --silent
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"}
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "Rufus portable"
 		Name = "rufus"
+		Tag = "Other"
 		Code = {
 			$uri = "https://api.github.com/repos/pbatard/rufus/releases/latest"
 			$get = Invoke-RestMethod -uri $uri -Method Get -ErrorAction stop
@@ -154,9 +169,10 @@
    			iwr $data.browser_download_url -Useb -OutFile ([Environment]::GetFolderPath("Desktop") + ".\rufus.exe")
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "SophiApp Tweaker portable"
 		Name = "sophia"
+		Tag = "Tweaks"
 		Code = {
 			$uri = "https://api.github.com/repos/Sophia-Community/SophiApp/releases/latest"
 			$get = Invoke-RestMethod -uri $uri -Method Get -ErrorAction stop
@@ -165,14 +181,14 @@
 			Expand-Archive -ErrorAction 0 -Force ".\SophiApp.zip" ([Environment]::GetFolderPath("Desktop"))
 		}
 	}
-	@{
+	[pscustomobject]@{
 		Description = "Win 11 23H2 iso folder"
 		Name = "win"
+		Tag = "Other"
 		Code = {
-			$os = "Windows 11, version 23H2 [x64]"
 			$apps = "WindowsStore", "Purchase", "VCLibs", "Photos", "Notepad", "Terminal", "Installer"
 			$options = "AutoStart", "AddUpdates", "Cleanup", "ResetBase", "SkipISO", "SkipWinRE", "CustomList", "AutoExit"
-			$id = ((irm "https://uup.rg-adguard.net/api/GetVersion?id=1").versions | where name -eq $os).UpdateId
+			$id = (irm "https://api.uupdump.net/fetchupd.php?arch=amd64&ring=retail&build=22631.1").response.updateId
 			iwr -Useb -Uri "https://uupdump.net/get.php?id=$id&pack=ru-ru&edition=core" -Method "POST" -Body "autodl=2" -OutFile '.\UUP.zip'
 			Expand-Archive -ErrorAction 0 -Force '.\UUP.zip' '.\'
 			(gc ".\ConvertConfig.ini") -replace (' '), ('') | sc ".\ConvertConfig.ini"
