3c3760966e573989a12c108611b2577a89073db9
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-11-21 22:26:10 +0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -17,7 +17,7 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 
 #ПРОВЕРКА REGEDIT
 if (!(gp -ErrorAction 0 -Path Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Associations)) {
-	start-job {
+	start-job -Name "Checking policies" {
 		$policies = "Software\Microsoft\Windows\CurrentVersion\Policies\Associations"
 		reg add "HKCU\$policies" /v "LowRiskFileTypes" /t REG_SZ /d ".exe;.msi;.zip;" /f
 		start-sleep 5
@@ -30,63 +30,62 @@ if ((Get-AppxPackage Microsoft.DesktopAppInstaller).Version -lt [System.Version]
 		Start-Process conhost "powershell -ExecutionPolicy Bypass -Command &{cd '$pwd'; $($MyInvocation.line)}" -Verb RunAs
 		(get-process | where MainWindowTitle -eq $host.ui.RawUI.WindowTitle).id | where {taskkill /PID $_}
 	} else {
-		start-job {&([ScriptBlock]::Create((irm https://raw.githubusercontent.com/asheroto/winget-install/master/winget-install.ps1))) -Force -ForceClose} | out-null
+		start-job -Name "Installing winget" {
+			&([ScriptBlock]::Create((irm https://raw.githubusercontent.com/asheroto/winget-install/master/winget-install.ps1))) -Force -ForceClose
+		} | out-null
 	}
+}
+
+#ПРОВЕРКА ПРИЛОЖЕНИЙ
+if (!$data) {
+	Start-Job -Name "Loading apps" {
+		try {$data = &([ScriptBlock]::Create((irm uffemcev.github.io/utilities/apps.ps1)))}
+		catch {throw}
+		start-sleep 5
+	} | out-null
+	$data = &([ScriptBlock]::Create((irm uffemcev.github.io/utilities/apps.ps1)))
+}
+
+#ПРОВЕРКА ПАРАМЕТРОВ
+if ($apps) {
+	foreach ($tag in ($data.tag | Select -Unique))  {
+		if ($tag -in $apps) {($data | where tag -eq $tag).Name | foreach {$apps.add($_)}}
+		($apps | where {$_ -eq $tag}) | foreach {$apps.Remove($_)}
+	}
+	if ($apps -contains "all") {$apps = $data.Name}
+	$apps = [array]($apps | Sort-Object -unique)
+	cleaner
+	$menu = $true
 }
 
 #ОЖИДАНИЕ ПРОВЕРОК
-if (get-job | where State -eq "Running") {
-	for ($i = 0; $i -lt (get-job).count+1; $i++) {
+if (get-job) {
+	$job = get-job
+	for ($i = 0; $i -le $job.count; $i++) {
 		cleaner
-		"Preparing for work"
-		$Jobs = (get-job | where State -eq "Running").count
-		$Processed = [Math]::Round(($i) / (get-job).count * 49,0)
-		$Remaining = 49 - $Processed
-		$PercentProcessed = [Math]::Round(($i) / (get-job).count * 100,0)
-		$Percent = $PercentProcessed -replace ('^(\d{1})$'), ('  $_%') -replace ('^(\d{2})$'), (' $_%') -replace ('^(\d{3})$'), ('$_%')
-		"`n" + (color -text (" " * $Processed) -number 7) + (color -text ("$Percent") -number 7) + (color -text (" " * $Remaining) -number 100)
-		if ($Jobs -eq 0) {break}
-		while (($Jobs -eq (get-job | where State -eq "Running").count)) {start-sleep 1}
+		if ($i -lt $job.count) {$job[$i].Name} else {$job[$i-1].Name}
+		$processed = [Math]::Round(($i) / $job.count * 49,0)
+		$remaining = 49 - $processed
+		$percentProcessed = [Math]::Round(($i) / $job.count * 100,0)
+		$percent = $percentProcessed -replace ('^(\d{1})$'), ('  $_%') -replace ('^(\d{2})$'), (' $_%') -replace ('^(\d{3})$'), ('$_%')
+		"`n" + (color -text (" " * $processed) -number 7) + (color -text ("$percent") -number 7) + (color -text (" " * $remaining) -number 100)
+		$job[$i] | wait-job -ErrorAction 0 | out-null
 	}
- 	$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-}
-
-#ЗАГРУЗКА ПРИЛОЖЕНИЙ
-$data = &([ScriptBlock]::Create((irm uffemcev.github.io/utilities/apps.ps1)))
-for ($i = 0; $i -lt $data.count+1; $i++) {
-	cleaner
-	$Processed = [Math]::Round(($i) / $data.count * 49,0)
-	$Remaining = 49 - $Processed
-	$PercentProcessed = [Math]::Round(($i) / $data.count * 100,0)
-	$Percent = $PercentProcessed -replace ('^(\d{1})$'), ('  $_%') -replace ('^(\d{2})$'), (' $_%') -replace ('^(\d{3})$'), ('$_%')
-	"Loading apps list"
-	"`n" + (color -text (" " * $Processed) -number 7) + (color -text ("$Percent") -number 7) + (color -text (" " * $Remaining) -number 100)
-	start-sleep -Milliseconds 100
+	$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+	get-job | remove-job | out-null
+	start-sleep 1
 }
 
 #НАЧАЛО РАБОТЫ
 winget settings --enable InstallerHashOverride | out-null
 ri -Recurse -Force -ErrorAction 0 ([System.IO.Path]::GetTempPath())
 cd ([System.IO.Path]::GetTempPath())
-get-job | remove-job | out-null
 $tagItems = [array]'All' + ($data.tag | select -Unique) + [array]'Confirm'
 $ypos = -1
 $xpos = 0
 
 #МЕНЮ
-:menu while ($true) {
-	
-	#ПРОВЕРКА ПАРАМЕТРОВ
-	$compare = Compare $apps $tagItems -Exclude -Include
-	if ($compare) {
-		foreach ($tag in ($data.tag | Select -Unique))  {
-			if ($apps -contains $tag) {($data | where tag -eq $tag).Name | foreach {$apps.add($_)}}
-			($apps | where {$_ -eq $tag}) | foreach {$apps.Remove($_)}
-		}
-		if ($apps -contains "all") {$apps = $data.Name}
-		cleaner
-		break menu
-	}
+while ($menu -ne $true) {
 	
 	#ПОДСЧЕТ
 	cleaner
@@ -133,7 +132,7 @@ $xpos = 0
 			} else {
 				switch ($tagItems[$xpos]) {
 					"All" {if ($apps) {$apps = @()} else {$apps = $data.Name}}
-					"Confirm" {cleaner; break menu}
+					"Confirm" {cleaner; $menu = $true}
 					DEFAULT {
 						$names = ($data | where Tag -eq $tagItems[$xpos]).name
 						$compare = Compare $apps $names -Exclude -Include
@@ -150,24 +149,23 @@ $xpos = 0
 	if (($ypos -lt 0) -or ($ypos -ge $appItems.count)) {$ypos = -1}
 }
 
+#ПРОВЕРКА ВЫХОДА
+if ($apps.count -eq 0) {$install = $true}
+
 #УСТАНОВКА
-:install while ($true) {
-
-	#ПРОВЕРКА ВЫХОДА
-	if ($apps.count -eq 0) {break install}
-
-	for ($i = 0; $i -lt $apps.count+1; $i++) {
+while ($install -ne $true) {
+	for ($i = 0; $i -le $apps.count; $i++) {
 		#ЗАПУСК
 		Get-job | Wait-Job | out-null
 		try {Start-Job -Name (($data | Where Name -eq $apps[$i]).Description) -Init ([ScriptBlock]::Create("cd '$pwd'")) -ScriptBlock $(($data | Where Name -eq $apps[$i]).Code) | out-null}
-		catch {Start-Job -Name ($apps[$i]) -ScriptBlock {throw} | out-null}
+		catch {Start-Job -Name ($apps[$i]) -ScriptBlock {start-sleep 1; throw} | out-null}
 		
 		#ПОДСЧЕТ
-		$Processed = [Math]::Round(($i) / $apps.count * 49,0)
-		$Remaining = 49 - $Processed
-		$PercentProcessed = [Math]::Round(($i) / $apps.count * 100,0)
-		$Percent = $PercentProcessed -replace ('^(\d{1})$'), ('  $_%') -replace ('^(\d{2})$'), (' $_%') -replace ('^(\d{3})$'), ('$_%')
-		$table = $apps | foreach {if ($_ -in $data.name) {($data | where Name -eq $_).Description} else {$_}} | Select @{Name="Name"; Expression={$_}}, @{Name="State"; Expression={
+		$processed = [Math]::Round(($i) / $apps.count * 49,0)
+		$remaining = 49 - $Processed
+		$percentProcessed = [Math]::Round(($i) / $apps.count * 100,0)
+		$percent = $percentProcessed -replace ('^(\d{1})$'), ('  $_%') -replace ('^(\d{2})$'), (' $_%') -replace ('^(\d{3})$'), ('$_%')
+		[array]$menu = $apps | foreach {if ($_ -in $data.name) {($data | where Name -eq $_).Description} else {$_}} | Select @{Name="Name"; Expression={$_}}, @{Name="State"; Expression={
 			switch ((get-job -name $_).State) {
 				"Running" {'Running'}
 				"Completed" {'Completed'}
@@ -178,11 +176,12 @@ $xpos = 0
 
 		#ВЫВОД
 		cleaner
-		($table | ft @{Expression={$_.Name}; Width=37; Alignment="Left"}, @{Expression={$_.State}; Width=15; Alignment="Right"} -HideTableHeaders | Out-String).Trim() + "`n"
-		(color -text (" " * $Processed) -number 7) + (color -text ("$Percent") -number 7) + (color -text (" " * $Remaining) -number 100)
+		if (($i -gt 9) -and ($i -lt $apps.count)) {$pos++}
+		($menu[$pos..($pos+9)] | ft @{Expression={$_.Name}; Width=37; Alignment="Left"}, @{Expression={$_.State}; Width=15; Alignment="Right"} -HideTableHeaders | Out-String).Trim()
+		"`n" + (color -text (" " * $Processed) -number 7) + (color -text ("$Percent") -number 7) + (color -text (" " * $Remaining) -number 100)
 	}
 	start-sleep 5
-	break install
+	$install = $true
 }
 
 #ЗАВЕРШЕНИЕ РАБОТЫ
