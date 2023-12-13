96f627dacbaa2d8ba1669c70ff19a03ba857bd62
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-12-14 02:12:33 +0700

    Update apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -69,11 +69,11 @@
 		}
 	}
 	[pscustomobject]@{
-		Description = "Vesktop - discord modified client"
-		Name = "vesktop"
+		Description = "Vencord - discord modified client"
+		Name = "vencord"
 		Tag = "Programs"
 		Code = {
-			$id = "Vencord.Vesktop"
+			$id = "Vendicated.Vencord"
 			winget install --id=$id --accept-package-agreements --accept-source-agreements --exact --silent
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"}
 		}
