b95a02861567941a7eba0712fd029c94f7b784ae
uffemcev <96709114+uffemcev@users.noreply.github.com>
2024-04-18 21:19:33 +0700

    Update and rename script1.ps1 to script3.ps1

--- script3.ps1
+++ script3.ps1
@@ -2,7 +2,7 @@ get-ExecutionPolicy
 pause
 import-module microsoft.powershell.security
 pause
-import-module -Name 'Microsoft.PowerShell.Security' -RequiredVersion 3.0.0.0
+#import-module -Name 'Microsoft.PowerShell.Security' -RequiredVersion 3.0.0.0
 if ((get-ExecutionPolicy) -ne 'bypass') {Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force}
 get-ExecutionPolicy
 pause
