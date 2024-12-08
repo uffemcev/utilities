2fb64a0e78c85d660f6f937df417b6d7af0a568b
uffemcev <96709114+uffemcev@users.noreply.github.com>
2024-12-08 13:54:59 +0700

    Update apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -239,7 +239,7 @@
 		Tag = "TEST"
 		Code = {
 			$id = "Wagnardsoft.DisplayDriverUninstaller"
-   			$run = "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"
+   			$run = "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact"
 			iex $run
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "$run"}
 		}
