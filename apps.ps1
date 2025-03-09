9db915ae12704e2b04826d96300292391cc8da4a
uffemcev <96709114+uffemcev@users.noreply.github.com>
2025-03-09 19:51:05 +0700

    Update apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -183,7 +183,7 @@
 		Tag = "other"
 		Code = {
 			$apps = "WindowsStore", "Purchase", "VCLibs", "Photos", "Notepad", "Terminal", "Installer"
-			$options = "AutoStart", "AddUpdates", "Cleanup", "ResetBase", "SkipWinRE", "CustomList", "AutoExit"
+			$options = "AutoStart", "AddUpdates", "Cleanup", "ResetBase", "wim2esd" "SkipWinRE", "CustomList", "AutoExit"
 			while (!(dir -errorAction 0 ".\UUP.zip")) {
 				try {
 					$id = ((irm "https://api.uupdump.net/fetchupd.php?arch=amd64&ring=retail&build=26100.1&pack=ru-ru").response.updateArray | Where updateTitle -match "^Windows 11.*$" | Sort -Descending -Property foundBuild | Select -First 1).updateId
