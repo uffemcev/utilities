268e686f4ceadf35221178ddf3b459aca477e2ee
uffemcev <96709114+uffemcev@users.noreply.github.com>
2025-06-04 23:41:57 +0700

    Discord

--- apps.ps1
+++ apps.ps1
@@ -36,11 +36,11 @@
 		}
 	}
 	[pscustomobject]@{
-		Description = "Vencord - modified Discord app"
-		Name = "vencord"
-		Tag = "audio"
+		Description = "Discord"
+		Name = "discord"
+		Tag = "games"
 		Code = {
-			$id = "Vendicated.Vencord"
+			$id = "Discord.Discord"
 			$run = "winget install --id=$id --accept-package-agreements --accept-source-agreements --ignore-security-hash --exact --silent"
 			iex $run
 			if (!((winget list) -match $id)) {runas /trustlevel:0x20000 /machine:amd64 "$run"}
